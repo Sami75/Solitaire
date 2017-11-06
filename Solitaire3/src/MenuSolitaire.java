72edbc61838a19b6962faefcf8fd93c18d578e0d
sthairi <sthairi@A35-11.MDNPEDAGO.local>
2017-11-06 11:47:37 +0100

    Patch 2.0.1 : * Changement menu *Début du partage du code (séparation des opération, affichage, ...)

--- Solitaire3/src/MenuSolitaire.java
+++ Solitaire3/src/MenuSolitaire.java
@@ -8,13 +8,8 @@ public void afficheMenu() {
 		Jeu a = new Jeu();
 		System.out.println();
 		System.out.println("1. Piocher");
-		System.out.println("2. Sélectionner la colonne 1");
-		System.out.println("3. Sélectionner la colonne 2");
-		System.out.println("4. Sélectionner la colonne 3");
-		System.out.println("5. Sélectionner la colonne 4");
-		System.out.println("6. Sélectionner la colonne 5");
-		System.out.println("7. Sélectionner la colonne 6");
-		System.out.println("8. Sélectionner la colonne 7");
+		System.out.println("2. Déplacement de carte");
+		System.out.println("3. Déplacement de pile");
 		System.out.println("Quel action souhaitez vous faire ? Taper le numéros correspondant");
 		
 		Scanner sc = new Scanner(System.in);

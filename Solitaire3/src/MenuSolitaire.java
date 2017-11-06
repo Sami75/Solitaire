98452269e28aa99eca37d7ed3b663f4c61916963
glimmois <glimmois@A35-10.MDNPEDAGO.local>
2017-11-06 13:42:50 +0100

    ok

--- Solitaire3/src/MenuSolitaire.java
+++ Solitaire3/src/MenuSolitaire.java
@@ -2,19 +2,12 @@ import java.util.Scanner;
 
 public class MenuSolitaire {
 
-	
-	
 public void afficheMenu() {
 		Jeu a = new Jeu();
 		System.out.println();
 		System.out.println("1. Piocher");
-		System.out.println("2. Sélectionner la colonne 1");
-		System.out.println("3. Sélectionner la colonne 2");
-		System.out.println("4. Sélectionner la colonne 3");
-		System.out.println("5. Sélectionner la colonne 4");
-		System.out.println("6. Sélectionner la colonne 5");
-		System.out.println("7. Sélectionner la colonne 6");
-		System.out.println("8. Sélectionner la colonne 7");
+		System.out.println("2. Déplacement de carte");
+		System.out.println("3. Déplacement de pile");
 		System.out.println("Quel action souhaitez vous faire ? Taper le numéros correspondant");
 		
 		Scanner sc = new Scanner(System.in);
@@ -24,19 +17,18 @@ public void afficheMenu() {
 		
 		case 1 : 
 				
-				a.pioche();
+				//a.pioche();
 				break;
 		
 		case 2 : 
-				a.deplaceColonne();
+				//a.deplaceColonne();
 				break;
 		
 		case 3 : 
-				a.deplacePile();
+				//a.deplacePile();
 				break;
 		
 		default : System.out.println("Veuillez entrer les numéros correspondant"); 
-		//new MenuSolitaire();
 		}
 	}
 }
